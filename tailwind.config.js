fe743654119be722790b78291bbda25f0f173c3b
Darin Hajou <darin.hajou@gmail.com>
2025-05-22 18:51:23 +0200

    Add custom Tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx}",
-    ],
-    theme: {
-      extend: {},
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'soft-black': '#1E1E1E',
+        'warm-white': '#FAF9F6',
+        'mist-blue': '#CBD8E0',
+        'glow-amber': '#F2CBA0',
+        'pale-sage': '#D9E2D2',
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
