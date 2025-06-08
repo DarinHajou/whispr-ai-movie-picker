942f4ba082501a7afc34d2d7fc82cd4c0baa2eab
Darin Hajou <darin.hajou@gmail.com>
2025-06-08 18:53:37 +0200

    Set up ROLLUP_NO_NATIVE variable early in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+// This disables native Rollup bindings at runtime
+process.env.ROLLUP_NO_NATIVE = '1';
+
+export default defineConfig({
   plugins: [react()],
-};
+});
