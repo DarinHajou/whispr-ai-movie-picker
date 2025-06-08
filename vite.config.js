dfd3cff28caa5f256bf8fbf257c90a133b73735a
Darin Hajou <darin.hajou@gmail.com>
2025-06-08 19:10:06 +0200

    test commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// This disables native Rollup bindings at runtime
-process.env.ROLLUP_NO_NATIVE = '1';
-
-export default defineConfig({
+export default {
   plugins: [react()],
-});
+};
