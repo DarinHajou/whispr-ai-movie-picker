bf485d32a19521049915532f351182988a7a3fbd
Darin Hajou <DarinHajou@gmail.com>
2025-08-01 00:04:13 +0200

    Add EmotionPicker stub

---
+++ src/components/EmotionPicker.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+export default function EmotionPicker({ onNext }) {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <p className="mb-4 text-warm-white">[EmotionPicker Stub]</p>
+      <button
+        onClick={onNext}
+        className="px-6 py-3 bg-bright-amber text-soft-black rounded-full"
+      >
+        Next
+      </button>
+    </div>
+  );
+}
