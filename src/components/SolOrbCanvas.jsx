3c8e0aaec5eefcb3767513946573298fc4e9b8e6
Darin Hajou <DarinHajou@gmail.com>
2025-08-04 21:09:27 +0200

    Swap old SolOrb for SolSoulCloud; enhance lighting in orb canvas

--- src/components/SolOrbCanvas.jsx
+++ src/components/SolOrbCanvas.jsx
@@ -1,25 +1,16 @@
 // src/components/SolOrbCanvas.jsx
-import React from "react";
+import SolSoulCloud from './SolSoulCloud';
 import { Canvas } from "@react-three/fiber";
 import SolOrb from "./SolOrb";
 
 export default function SolOrbCanvas({ pulse }) {
   console.log("Canvas got pulse:", pulse);
   return (
-    <Canvas
-      gl={{ alpha: true }}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-      }}
-      camera={{ position: [0, 0, 5], fov: 60 }}
-    >
-      <ambientLight intensity={0.5} />
-      <pointLight position={[2, 2, 2]} />
-      <SolOrb pulse={pulse} />
-    </Canvas>
+   <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
+      <ambientLight intensity={0.7} />
+      <pointLight position={[2, 2, 3]} intensity={1.6} />
+      <SolSoulCloud pulse={pulse} />
+   </Canvas>
+
   );
 }
